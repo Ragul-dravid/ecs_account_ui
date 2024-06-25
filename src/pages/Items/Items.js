67c29a175768aa93cbbc558cc8e24920d124067c
Chandru0811 <chandru08112000@gmail.com>
2024-06-25 17:51:14 +0530

    Item committed

--- src/pages/Items/Items.js
+++ src/pages/Items/Items.js
@@ -3,7 +3,6 @@ import "datatables.net-dt";
 import "datatables.net-responsive-dt";
 import $ from "jquery";
 import { Link } from "react-router-dom";
-import { FaEye, FaEdit } from "react-icons/fa";
 import DeleteModel from "../../components/common/DeleteModel";
 
 const Items = () => {
