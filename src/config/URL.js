cc562d387b51805627388ae450f02e3a162c021e
Chandru0811 <chandru08112000@gmail.com>
2024-06-28 13:23:05 +0530

    Base url added

---
+++ src/config/URL.js
@@ -0,0 +1,24 @@
+// api.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://13.213.208.92:7083/ecsacc/api/",
+});
+
+// Add a request interceptor
+api.interceptors.request.use(
+  function (config) {
+    const token = sessionStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
