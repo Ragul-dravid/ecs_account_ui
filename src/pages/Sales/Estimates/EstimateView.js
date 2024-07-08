959b4cd93306fd25f3333d11aa8f4a5a98cdc918
manojecs2020 <manoj@ecscloudinfotech.com>
2024-07-08 19:10:02 +0530

    bug fixed

--- src/pages/Sales/Estimates/EstimateView.js
+++ src/pages/Sales/Estimates/EstimateView.js
@@ -1,197 +1,210 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import Logo from "../../../assets/AccountsLogo.png"
-import { IoCloudDownloadSharp } from "react-icons/io5";
-import { FaTelegramPlane } from "react-icons/fa";
-import api from "../../../config/URL";
+import Logo from "../../../assets/AccountsLogo.png";
 import toast from "react-hot-toast";
+import api from "../../../config/URL"
 
-const EstimateView = () => {
+function EstimateView() {
+    const { id } = useParams();
+    const [data, setData] = useState([]);
+    const [items, setItems] = useState([]);
+    const [error, setError] = useState(null);
+    const [quotes, setQuotes] = useState(null);
 
-  const { id } = useParams();
-  const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchCreditNotes = async () => {
+            try {
+                const response = await api.get(`/getTxnQuotesById/${id}`);
+                setQuotes(response.data);
+            } catch (error) {
+                setError(error.message || "Failed to fetch data");
+                toast.error("Error fetching data: " + error.message);
+            }
+        };
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await api.get(`/getTxnQuotesById/${id}`);
-        setData(response.data);
-      } catch (error) {
-        toast.error("Error Fetching Data", error);
-      }
-    };
-    getData();
-  }, [id]);
-  return (
-    <div className="container-fluid px-2 minHeight">
-      <div className="card shadow border-0 mb-2 top-header">
-        <div className="container-fluid py-4">
-          <div className="row align-items-center">
-            <div className="col">
-              <div className="d-flex align-items-center gap-4">
-                <h1 className="h4 ls-tight headingColor">View Estimates</h1>
-              </div>
+        fetchCreditNotes();
+    }, [id]);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <div className="container-fluid px-2 minHeight">
+            <div className="card shadow border-0 mb-2 top-header">
+                <div className="container-fluid py-4">
+                    <div className="row align-items-center">
+                        <div className="col">
+                            <div className="d-flex align-items-center gap-4">
+                                <h1 className="h4 ls-tight headingColor">View Estimates</h1>
+                            </div>
+                        </div>
+                        <div className="col-auto">
+                            <div className="hstack gap-2 justify-content-start">
+                                <Link to="/estimates">
+                                    <button type="button" className="btn btn-light btn-sm me-1">
+                                        <span>Back</span>
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="col-auto">
-              <div className="hstack gap-2 justify-content-start">
-                <Link to="/estimates">
-                  <button type="submit" className="btn btn-sm btn-light">
-                    <span>Back</span>
-                  </button>
-                </Link>
-                <button type="submit" className="btn btn-button rounded-pill p-2">
-                  <FaTelegramPlane size={18} />
-                </button>
-                <button type="submit" className="btn btn-button rounded-pill p-2">
-                  <IoCloudDownloadSharp size={18} />
-                </button>
-                <button type="submit" className="btn btn-sm btn-button rounded">
-                  Save
-                </button>
-              </div>
+            <div className="card shadow border-0 mb-2 minHeight">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-12">
+                            <div className="d-flex justify-content-center flex-column align-items-start">
+                                <div className="d-flex">
+                                    <img
+                                        src={Logo}
+                                        alt="Company Logo"
+                                        className="mt-3"
+                                        width={130}
+                                    />
+                                </div>
+                                <p className="fw-small mt-2">
+                                    Cloud ECS Infotech Pte Ltd
+                                    <br />
+                                    Anna Salai
+                                    <br />
+                                    Chennai - 600002,
+                                    <br />
+                                    Tamil Nadu
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-12 d-flex justify-end flex-column align-items-end mt-2">
+                            {quotes && (
+                                <>
+                                    <h1>CREDIT NOTES</h1>
+                                    <h3>{quotes.quotes || "--"}</h3>
+                                    <span className="text-muted mt-4">Date</span>
+                                    <h3>{quotes.expiryDate || "N/A"}</h3>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-md-6 col-12">
+                            <div className="d-flex justify-content-center flex-column align-items-start">
+                                {quotes && (
+                                    <>
+                                        <h3>Customer Details</h3>
+                                        <span style={{ color: "#2196f3" }}>
+                                            : {quotes.customerName || ""}
+                                        </span>
+                                        <p className="fw-small">
+                                            : {quotes.customerAddress || "Address not available"}
+                                        </p>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-12 text-end">
+                            <div className="row mb-2 d-flex justify-content-end align-items-end">
+                                {quotes && (
+                                    <>
+                                        <div className="col-6">
+                                            <p className="text-sm">
+                                                <b>Reference</b>
+                                            </p>
+                                        </div>
+                                        <div className="col-6">
+                                            <p className="text-muted text-sm">
+                                                : {quotes.reference || "N/A"}
+                                            </p>
+                                        </div>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <h3 style={{ background: "#4066D5" }} className="text-light p-2">
+                            Item Table
+                        </h3>
+
+                        <div className="table-responsive">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">S.NO</th>
+                                        <th scope="col">ITEM </th>
+                                        <th scope="col">QUANTITY</th>
+                                        <th scope="col">PRICE</th>
+                                        <th scope="col">DISCOUNT</th>
+                                        <th scope="col">TAX RATE</th>
+                                        <th scope="col">AMOUNT</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="table-group">
+                                    {quotes &&
+                                        quotes.quotesItemsModels &&
+                                        quotes.quotesItemsModels.map((item, index) => (
+                                            <tr key={index}>
+                                                <th scope="row">{index + 1}</th>
+                                                <td>{item.item}</td>
+                                                <td>{item.qty}</td>
+                                                <td>{item.price}</td>
+                                                <td>{item.disc}</td>
+                                                <td>{item.taxRate}</td>
+                                                <td>{item.amount}</td>
+                                            </tr>
+                                        ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="col-md-6 col-12"></div>
+                        <div className="col-md-6 col-12">
+                            {quotes && (
+                                <>
+                                    <div className="row mb-3 mt-2">
+                                        <label className="col-sm-6 col-form-label">Sub Total</label>
+                                        <div className="col-sm-6">
+                                            ₹{quotes.subTotal || "0"}
+                                        </div>
+                                    </div>
+                                    <div className="row mb-3">
+                                        <label className="col-sm-6 col-form-label">Toatal Tax</label>
+                                        <div className="col-sm-6">
+                                            ₹{quotes.totalTax || "0"}
+                                        </div>
+                                    </div>
+                                    <div className="row mb-3">
+                                        <label className="col-sm-6 col-form-label">Total</label>
+                                        <div className="col-sm-6">₹{quotes.total || "0"}</div>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        {quotes && (
+                            <>
+                                <div className="col-md-6 col-12">
+                                    <div className="d-flex justify-content-center flex-column align-items-start">
+                                        <h3>Terms & Conditions</h3>
+                                        <p className="fw-small">
+                                            {quotes.terms ||
+                                                "Payment is due upon receipt of the invoice unless otherwise agreed upon in writing."}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-12">
+                                    <div className="d-flex justify-content-center flex-column align-items-start">
+                                        <h3>Customer Notes</h3>
+                                        <p className="fw-small">{quotes.cusNotes || ""}</p>
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-      <div className="card shadow border-0 mb-2 minHeight">
-        <div className="container-fluid mt-5">
-          <div className="row mt-4 p-3">
-            <div className="col-md-6 col-12 ">
-              <div className=" d-flex align-items-center">
-                <img src={Logo} alt="" className="mt-3" width={130} />
-              </div>
-              <div className="">
-              </div>
-              <p>Cloud ECS Infotech Pte Ltd</p>
-              <p>Anna Salai,</p>
-              <p>Chennai-600002,</p>
-              <p>Tamil Nadu</p></div>
-
-            <div className="col-md-6 col-12 d-flex flex-column align-items-end pt-5">
-              <h5>ESTIMATE</h5>
-              <p>#EST-018</p>
-            </div>
-          </div>
-          <div className="row mt-2 p-3">
-            <div className="mt-5 mb-2">
-              <h3>Bill To</h3>
-            </div>
-            <div className="col-md-6 col-12">
-              <p className="text-info">{data.customerName}</p>
-              <p>Purasaiwalkam,</p>
-              <p>Chennai-600002</p>
-              <p>Tamil Nadu</p>
-            </div>
-            <div className="col-md-6 col-12 text-end">
-              <div className="row my-3">
-                <div className="col-6 d-flex justify-content-end align-items-center">
-                  <p className="text-sm">
-                    <b>Estimate Date</b>
-                  </p>
-                </div>
-                <div className="col-6">
-                  <p className="text-muted text-sm">:  {new Date(data.issueDate).toLocaleDateString('en-GB')}</p>
-                </div>
-              </div>
-              <div className="row my-3">
-                <div className="col-6 d-flex justify-content-end align-items-center">
-                  <p className="text-sm">
-                    <b>reference #</b>
-                  </p>
-                </div>
-                <div className="col-6">
-                  <p className="text-muted text-sm">: {data.reference}</p>
-                </div>
-              </div>
-              <div className="row my-3">
-                <div className="col-6 d-flex justify-content-end align-items-center">
-                  <p className="text-sm">
-                    <b>Expiry Date</b>
-                  </p>
-                </div>
-                <div className="col-6">
-                  <p className="text-muted text-sm">:  {new Date(data.expiryDate).toLocaleDateString('en-GB')}</p>
-                </div>
-              </div>
-            </div>
-            <div className="mt-5 mb-2">
-              <h3>subject :</h3>
-            </div>
-            <div className="col-md-6 col-12 my-2">
-              <p>Full Stack Developer Training program</p>
-            </div>
-            <div className="">
-              <h3 style={{ background: "#4066D5" }} className="text-light p-2">
-                Item Table
-              </h3>
-            </div>
-            <div className="table-responsive">
-              <table class="table  ">
-                <thead>
-                  <tr>
-                    <th scope="col">S.NO</th>
-                    <th scope="col">ITEM DETAILS</th>
-                    <th scope="col">QUANTITY</th>
-                    <th scope="col">RATE</th>
-                    <th scope="col">DISCOUNT</th>
-                    <th scope="col">TAX</th>
-                    <th scope="col">AMOUNT</th>
-                  </tr>
-                </thead>
-                <tbody class="table-group">
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Apple</td>
-                    <td>1.00</td>
-                    <td>3500</td>
-                    <td>10</td>
-                    <td>Commission</td>
-                    <td>3500</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="row mt-5 p-3">
-            <div className="col-md-6 col-12">
-              <h5 className="fw-bolder my-2">Notes</h5>
-              <p className="my-3">Thanks for your business.</p>
-              <h5 className="fw-bolder my-2">Terms & Conditions</h5>
-              <p className="my-3">
-                {data.terms}
-              </p>
-              <p className="my-3">
-               {data.terms}
-              </p>
-            </div>
-            <div className="col-md-6 col-12 card shadow border-2 h-40 d-flex justify-content-center gap-5 ">
-              <div className="row text-center">
-                <div className="col-6 ">
-                  <p>Sub Total</p>
-                </div>
-                <div className="col-6">: {data.subTotal}</div>
-              </div>
-              <div className="row text-center">
-                <div className="col-6">
-                  <p>Total Tax</p>
-                </div>
-                <div className="col-6">: 0.00</div>
-              </div>
-              <div className="row text-center">
-                <div className="col-6">
-                  <p>Total (₹)</p>
-                </div>
-                <div className="col-6">: {data.total}</div>
-              </div>
-            </div>
-            <div className="col-12 my-5">
-              <p className="fw-bolder">Authorized Signature ___________________</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
 
 export default EstimateView;
